a71c1fe10fc3415abcbb825c2e3a8f8b799b1d33
JanHolger <jan@bebendorf.eu>
2019-01-25 22:25:05 +0100

    Fixed event bus

--- src/main/java/eu/bebendorf/ajorm/event/OnDelete.java
+++ src/main/java/eu/bebendorf/ajorm/event/OnDelete.java
@@ -1,5 +1,12 @@
 package eu.bebendorf.ajorm.event;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
 public @interface OnDelete {
     boolean after() default false;
 }
