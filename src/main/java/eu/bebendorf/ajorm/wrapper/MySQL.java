cfe17894f9c960b66889f199a8a84ebe0ce5d4a8
JanHolger <jan@bebendorf.eu>
2020-06-27 12:18:57 +0200

    Added a setNetworkTimeout call in the MySQL Implementation to fix the timeout exceptions

--- src/main/java/eu/bebendorf/ajorm/wrapper/MySQL.java
+++ src/main/java/eu/bebendorf/ajorm/wrapper/MySQL.java
@@ -3,6 +3,8 @@ package eu.bebendorf.ajorm.wrapper;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
 public class MySQL extends BaseSQL {
 
@@ -12,14 +14,21 @@ public class MySQL extends BaseSQL {
     private String database;
     private String username;
     private String password;
+    private Executor timeoutExecutor;
+    private int timeout;
 
+    public MySQL(String host, int port, String database, String username, String password) {
+        this(host, port, database, username, password, 60);
+    }
 
-    public MySQL(String ho, int po, String db, String un, String pw) {
-        host=ho;
-        port=po;
-        database=db;
-        username=un;
-        password=pw;
+    public MySQL(String host, int port, String database, String username, String password, int timeout) {
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.username = username;
+        this.password = password;
+        this.timeout = timeout;
+        timeoutExecutor = Executors.newSingleThreadExecutor();
     }
 
     public Connection getConnection(){
@@ -28,6 +37,7 @@ public class MySQL extends BaseSQL {
                 try {
                     Class.forName("com.mysql.jdbc.Driver");
                     c = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database + "?user=" + this.username + "&password=" + this.password + "&autoReconnect=" + true + "&failOverReadOnly=false&maxReconnects=" + 5 + "&UseUnicode=yes&characterEncoding=UTF-8");
+                    c.setNetworkTimeout(timeoutExecutor, timeout);
                 } catch (SQLException e) {
                     System.out.println("Fehler: bei getConnection()[MySQL.java]  SQLException   " + e.getMessage());
                 } catch (ClassNotFoundException e) {
