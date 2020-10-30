0f0c4e30038bcc66d00338545317ed07cd46e19b
JanHolger <jan@bebendorf.eu>
2020-10-30 10:12:02 +0100

    Added timestamps to migration

--- src/main/java/org/javawebstack/orm/migration/DB.java
+++ src/main/java/org/javawebstack/orm/migration/DB.java
@@ -52,35 +52,62 @@ public class DB {
         public Column string(String name){
             return string(name, 255);
         }
+
         public Column string(String name, int size){
             return column(name, SQLType.VARCHAR, String.valueOf(size));
         }
+
         public Column text(String name, int size){
             return column(name, SQLType.TEXT, String.valueOf(size));
         }
+
         public Column text(String name){
             return column(name, SQLType.TEXT);
         }
+
         public Column id(){
             return integer("id").autoIncrement().primary();
         }
+
         public Column integer(String name, int size){
             return column(name, SQLType.INT, String.valueOf(size));
         }
+
         public Column integer(String name){
             return column(name, SQLType.INT);
         }
+
         public Column bool(String name){
             return column(name, SQLType.TINYINT, "1");
         }
+
         public Column uuid(String name){
             return column(name, SQLType.VARCHAR, "36");
         }
+
         public Column uuid(){
             return uuid("uuid").primary();
         }
+
         public Column enums(String name, String... values){
             return column(name, SQLType.ENUM, String.join(",", values));
+        }
+
+        public Column timestamp(String name){
+            return column(name, SQLType.TIMESTAMP);
+        }
+
+        public void timestamps(String... names){
+            for(String name : names)
+                timestamp(name);
+        }
+
+        public void dates(){
+            timestamps("created_at", "updated_at");
+        }
+
+        public void softDelete(){
+            timestamp("deleted_at").nullable();
         }
 
         public void rename(String to){
@@ -119,43 +146,53 @@ public class DB {
             private String name;
             private final SQLType type;
             private final String size;
+
             public Column(String name, SQLType type, String size){
                 this.name = name;
                 this.type = type;
                 this.size = size;
             }
+
             public Column primary(){
                 this.primary = true;
                 return this;
             }
+
             public Column unique(){
                 this.unique = true;
                 return this;
             }
+
             public Column autoIncrement(){
                 this.autoIncrement = true;
                 return this;
             }
+
             public Column nullable(){
                 this.nullable = true;
                 return this;
             }
+
             public Column after(String after){
                 this.after = after;
                 return this;
             }
+
             public Column first(){
                 this.after = "";
                 return this;
             }
+
             public void rename(String to){
                 sql.write("ALTER TABLE `"+fullName()+"` RENAME COLUMN `"+name+"` TO `"+to+"`;");
                 name = to;
             }
+
             public void drop(){
                 sql.write("ALTER TABLE `"+fullName()+"` DROP COLUMN `"+name+"`;");
                 columns.remove(this);
             }
+
             public void add(){
                 StringBuilder sb = new StringBuilder("ALTER TABLE `");
                 sb.append(fullName());
@@ -164,6 +201,7 @@ public class DB {
                 sb.append(';');
                 sql.write(sb.toString());
             }
+
             public void modify(){
                 StringBuilder sb = new StringBuilder("ALTER TABLE `");
                 sb.append(fullName());
@@ -172,6 +210,7 @@ public class DB {
                 sb.append(';');
                 sql.write(sb.toString());
             }
+            
             String definition(){
                 StringBuilder sb = new StringBuilder('`');
                 sb.append(name);
