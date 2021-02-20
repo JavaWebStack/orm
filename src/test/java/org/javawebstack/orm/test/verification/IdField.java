4d42f8712b4be6df7bd930da769e0bd9f2f55c22
Timothy Gillespie <t.gillespie@lumaserv.com>
2021-02-20 15:20:23 +0100

    Fix weird parameter name

--- src/test/java/org/javawebstack/orm/test/verification/IdField.java
+++ src/test/java/org/javawebstack/orm/test/verification/IdField.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 // Extending the ORMTestCase Class to obtain the database connection
 public class IdField extends ORMTestCase {
 
-    public static void assertCorrectDatabaseFormat(String table_name) throws SQLException {
-        Field field = new Field(table_name, "id");
+    public static void assertCorrectDatabaseFormat(String tableName) throws SQLException {
+        Field field = new Field(tableName, "id");
 
         field.assertAutoIncrementing();
         field.assertNotNullable();
