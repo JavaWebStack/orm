6aeb181e444cdbbdd8a12068e8565c1814bd2b13
Timothy Gillespie <t.gillespie@lumaserv.com>
2021-02-20 16:33:40 +0100

    Add SMALLINT

--- src/main/java/org/javawebstack/orm/SQLType.java
+++ src/main/java/org/javawebstack/orm/SQLType.java
@@ -7,6 +7,9 @@ public enum SQLType {
 
     // Divided by category and ordered by byte size (MySQL sizes)
     TINYINT(Boolean.class),
+    SMALLINT(Short.class),
+    // Not in use for any java data type
+    //MEDIUMINT(),
     INT(Integer.class),
     BIGINT(Long.class),
 
