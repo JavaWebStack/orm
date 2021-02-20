f8d346f2f16205b1667ff7c2b1411777ec6b7959
Timothy Gillespie <t.gillespie@lumaserv.com>
2021-02-20 01:07:39 +0100

    Change test set up over to property based environment

--- src/test/java/org/javawebstack/orm/test/ORMTestCase.java
+++ src/test/java/org/javawebstack/orm/test/ORMTestCase.java
@@ -5,17 +5,33 @@ import org.javawebstack.orm.Repo;
 import org.javawebstack.orm.migration.AutoMigrator;
 import org.javawebstack.orm.wrapper.MySQL;
 import org.javawebstack.orm.wrapper.SQL;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 
 public abstract class ORMTestCase {
 
     private static final SQL sql;
 
     static {
-        String host = System.getenv("MYSQL_HOST");
-        String port = System.getenv("MYSQL_PORT");
-        String name = System.getenv("MYSQL_DATABASE");
-        String user = System.getenv("MYSQL_USERNAME");
-        String password = System.getenv("MYSQL_PASSWORD");
+        Properties properties = new Properties();
+        File propertiesFile = new File("testenv.properties");
+        if(propertiesFile.exists()) {
+            try {
+                properties.load(new FileInputStream(propertiesFile));
+            } catch (IOException ignored) {}
+        }
+
+        String host = properties.getProperty("mysql.host", System.getenv("MYSQL_HOST"));
+        String port = properties.getProperty("mysql.port", System.getenv("MYSQL_PORT"));
+        String name = properties.getProperty("mysql.database", System.getenv("MYSQL_DATABASE"));
+        String user = properties.getProperty("mysql.username", System.getenv("MYSQL_USERNAME"));
+        String password = properties.getProperty("mysql.password", System.getenv("MYSQL_PASSWORD"));
+
         sql = new MySQL(
                 host != null ? host : "localhost",
                 port != null ? Integer.parseInt(port) : 3306,
@@ -25,11 +41,17 @@ public abstract class ORMTestCase {
         );
     }
 
-    {
+    @BeforeEach
+    public void beforeEach() {
         reset();
     }
 
-    protected static void reset() {
+    @AfterAll
+    public static void finishTestClass() {
+//        reset();
+    }
+
+    public static void reset() {
         AutoMigrator.drop(ORM.getRepos().toArray(new Repo[0]));
         ORM.reset();
     }
