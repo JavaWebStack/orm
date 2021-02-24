6451e4dfa95e3eb34911a99cabf1434cc28d5c72
Timothy Gillespie <t.gillespie@lumaserv.com>
2021-02-24 14:01:22 +0100

    Take byte overhead into account for the max sizes in the tests

--- src/test/java/org/javawebstack/orm/test/automigrate/DefaultSizeTest.java
+++ src/test/java/org/javawebstack/orm/test/automigrate/DefaultSizeTest.java
@@ -16,15 +16,54 @@ import java.sql.SQLException;
 
 public class DefaultSizeTest extends ORMTestCase {
 
+    // Copy from DefaultMapper:
+    // SOURCES:
+    // TEXT Datatypes: https://www.mysqltutorial.org/mysql-text/
+    private static final int BYTES_OVERHEAD_VARCHAR = 4;
+    private static final int BYTES_OVERHEAD_TINYTEXT = 1;
+    private static final int BYTES_OVERHEAD_TEXT = 2;
+    private static final int BYTES_OVERHEAD_MEDIUMTEXT = 3;
+    private static final int BYTES_OVERHEAD_LONGTEXT = 4;
+
+    // The max sizes given in the manual are in bytes. There are overheads which need to be subtracted.
+    // The following values assume utf8mb4 encoding which uses 4 bytes per character and
+    // further quarters the maximum column length accordingly.
+    private static final long MAX_SIZE_VARCHAR = (long) Math.floor((65535 - BYTES_OVERHEAD_VARCHAR) / 4);
+    private static final long MAX_SIZE_MEDIUMTEXT = (long) Math.floor((16777215 - BYTES_OVERHEAD_MEDIUMTEXT) / 4);
+    private static final long MAX_SIZE_LONGTEXT = (long) Math.floor((4294967295L - BYTES_OVERHEAD_LONGTEXT) / 4);
+
     final static String tableName = "just_strings";
 
+    @Test
+    public void testStringUsesDefaultSizeChar() throws ORMConfigurationException, SQLException {
+        setUpWithDefaultSize(JustString.class, 1);
+        (new Field("just_strings", "string")).assertType("char(1)");
+    }
 
     @Test
-    public void testStringUsesDefaultSize() throws ORMConfigurationException, SQLException {
-        int[] parameters = {2, 3, 100, 254, 255, 256, 65534, 65535};
+    public void testStringUsesDefaultSizeVarchar() throws ORMConfigurationException, SQLException {
+        int[] parameters = {2, 3, 123, 1234, 12345, (int) MAX_SIZE_VARCHAR - 1, (int) MAX_SIZE_VARCHAR};
         for( int singleParameter : parameters) {
             setUpWithDefaultSize(JustString.class, singleParameter);
             (new Field("just_strings", "string")).assertType(String.format("varchar(%s)", singleParameter));
+        }
+    }
+
+    @Test
+    public void testStringUsesDefaultSizeMediumText() throws ORMConfigurationException, SQLException {
+        int[] parameters = {(int) MAX_SIZE_VARCHAR + 1, 123456, 1234567, (int) MAX_SIZE_MEDIUMTEXT - 1};
+        for( int singleParameter : parameters) {
+            setUpWithDefaultSize(JustString.class, singleParameter);
+            (new Field("just_strings", "string")).assertType("mediumtext");
+        }
+    }
+
+    @Test
+    public void testStringUsesDefaultSizeLongText() throws ORMConfigurationException, SQLException {
+        int[] parameters = {(int) MAX_SIZE_MEDIUMTEXT + 1, 123456789, 123467890, Integer.MAX_VALUE};
+        for( int singleParameter : parameters) {
+            setUpWithDefaultSize(JustString.class, singleParameter);
+            (new Field("just_strings", "string")).assertType("longtext");
         }
     }
 
