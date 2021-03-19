823f16cccdbcb055b03c057e668192e656801f7e
Timothy Gillespie <Timothy.Gillespie@outlook.de>
2021-03-20 00:00:14 +0100

    Revert "Reduce boilerplate code further"
    This reverts commit 345372b2f2828af0141e497a97ac5692493d9ed2.

--- src/test/java/org/javawebstack/orm/test/shared/models/OnlyIdModel.java
+++
@@ -1,9 +0,0 @@
-package org.javawebstack.orm.test.shared.models;
-
-import org.javawebstack.orm.Model;
-import org.javawebstack.orm.annotation.Column;
-
-public class OnlyIdModel extends Model {
-    @Column
-    int id;
-}
