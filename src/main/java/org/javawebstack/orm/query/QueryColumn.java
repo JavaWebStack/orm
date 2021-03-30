478ef588db13bd5bfdba73fe7bfc2a533463ff75
JanHolger <jan@bebendorf.eu>
2021-03-30 20:28:09 +0200

    Added validation for operators and non raw column names

--- src/main/java/org/javawebstack/orm/query/QueryColumn.java
+++ src/main/java/org/javawebstack/orm/query/QueryColumn.java
@@ -1,11 +1,15 @@
 package org.javawebstack.orm.query;
 
 import org.javawebstack.orm.TableInfo;
+import org.javawebstack.orm.exception.ORMQueryException;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class QueryColumn {
+
+    private static final Pattern NAME_PATTERN = Pattern.compile("[A-Za-z0-9_-]+");
 
     private final String name;
     private final boolean raw;
@@ -15,6 +19,8 @@ public class QueryColumn {
     }
 
     public QueryColumn(String name, boolean raw) {
+        if(!raw)
+            validateName(name);
         this.name = name;
         this.raw = raw;
     }
@@ -35,6 +41,11 @@ public class QueryColumn {
         if (raw)
             return name;
         return Arrays.stream((info != null ? info.getColumnName(name) : name).split("\\.")).map(s -> "`" + s + "`").collect(Collectors.joining("."));
+    }
+
+    private static void validateName(String name) {
+        if(!NAME_PATTERN.matcher(name).matches())
+            throw new ORMQueryException("Invalid column name '" + name + "' (Use raw in case you know what you're doing)");
     }
 
 }
--- src/main/java/org/javawebstack/orm/query/QueryCondition.java
+++ src/main/java/org/javawebstack/orm/query/QueryCondition.java
@@ -1,6 +1,31 @@
 package org.javawebstack.orm.query;
 
+import org.javawebstack.orm.exception.ORMQueryException;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
 public class QueryCondition implements QueryElement {
+
+    private static final List<String> VALID_OPERATORS = Arrays.asList(
+            "=",
+            "<=>",
+            "!=",
+            "<>",
+            "<=",
+            ">=",
+            "<",
+            ">",
+            "is null",
+            "is not null",
+            "is",
+            "is not",
+            "in",
+            "not in",
+            "like",
+            "not like"
+    );
 
     private final Object left;
     private final String operator;
@@ -8,8 +33,9 @@ public class QueryCondition implements QueryElement {
     private final boolean not;
 
     public QueryCondition(Object left, String operator, Object right, boolean not) {
+        validateOperator(operator);
         this.left = left;
-        this.operator = operator; // TODO Validate and throw exception
+        this.operator = operator;
         this.right = right;
         this.not = not;
     }
@@ -36,6 +62,11 @@ public class QueryCondition implements QueryElement {
 
     public boolean isNot() {
         return not;
+    }
+
+    private static void validateOperator(String operator) {
+        if(!VALID_OPERATORS.contains(operator.toLowerCase(Locale.ROOT)))
+            throw new ORMQueryException("The given operator '" + operator + "' is invalid or not supported");
     }
 
 }
