1ae8ac89ea87dcb3cbb58eaa115f5cd810339c1b
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-03-31 06:18:26 +0200

    Add retrieval methods for the sections

--- src/test/java/org/javawebstack/orm/test/shared/verification/QueryVerification.java
+++ src/test/java/org/javawebstack/orm/test/shared/verification/QueryVerification.java
@@ -1,9 +1,11 @@
 package org.javawebstack.orm.test.shared.verification;
 
 import org.javawebstack.orm.query.Query;
+import org.javawebstack.orm.test.exception.SectionIndexOutOfBoundException;
 import org.javawebstack.orm.test.shared.util.QueryStringUtil;
 
 import java.util.HashSet;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -51,30 +53,68 @@ public class QueryVerification {
      *
      * @param topLevelKeyword The top level keyword that prefaces the section.
      * @param containedSubstring The substring which should be contained in the first section of the given type.
-     * @param sectionIndex The index of the section to be checked, thusly 0 refers to the first occurrence etc.
+     * @param sectionIndex The index of the section to be checked, thus 0 refers to the first occurrence etc.
      */
     public void assertSectionContains(String topLevelKeyword, String containedSubstring, int sectionIndex) {
-        String sectionString;
-
+        String sectionString = null;
         try {
-            sectionString = new QueryStringUtil(this.query.getQueryString().getQuery())
-                    .getTopLevelSectionsByKeyword(topLevelKeyword)
-                    .get(sectionIndex);
-        } catch (IndexOutOfBoundsException ignored) {
+            sectionString = getSection(topLevelKeyword, sectionIndex);
+        } catch (SectionIndexOutOfBoundException e) {
             fail(String.format(
                 "A top level section of type %s and index %d was tested but only %d sections of that type existed.",
-                sectionIndex,
-                topLevelKeyword,
-                sectionIndex + 1
+                e.getTopLevelKeyword(),
+                e.getAttemptedIndex(),
+                e.getSectionCount()
             ));
-
-            return;
         }
 
         assertTrue(
             sectionString.contains(containedSubstring),
             String.format("The occurrence of index %d of %s section of the query did not contain a substring %s but looked like this: %s. Note that the match is case-sensitive.", sectionIndex, topLevelKeyword, containedSubstring, sectionString)
         );
+    }
+
+    /**
+     * Retrieves the inner part of a section by its keyword. With multiple occurrences it will only retrieve the first
+     * one. It does not include the keyword and one whitespaces at start and end.
+     *
+     * @param topLevelKeyword The top level keyword that prefaces the section.
+     * @return The inner part of the first section as specified.
+     * @throws SectionIndexOutOfBoundException if no section by that top level keyword exists.
+     */
+    public String getSection(String topLevelKeyword) throws SectionIndexOutOfBoundException {
+        return this.getSection(topLevelKeyword, 0);
+    }
+
+    /**
+     * Retrieves the inner part of a section by its keyword and index specifying which occurrence should be retrieved.
+     * It does not include the keyword and one whitespaces at start and end.
+     *
+     * @param topLevelKeyword The top level keyword that prefaces the section.
+     * @param sectionIndex The index of the section to be retrieved, thus 0 refers to the first occurrence etc.
+     * @return The inner part of the first section as specified.
+     * @throws SectionIndexOutOfBoundException if there are less than sectionIndex + 1 elements
+     */
+    public String getSection(String topLevelKeyword, int sectionIndex) throws SectionIndexOutOfBoundException {
+        List<String> sectionList = this.getSectionList(topLevelKeyword);
+        try {
+            return sectionList.get(sectionIndex);
+        } catch (IndexOutOfBoundsException converted) {
+
+            SectionIndexOutOfBoundException exception = new SectionIndexOutOfBoundException();
+
+            exception.setSectionCount(sectionList.size());
+            exception.setAttemptedIndex(sectionIndex);
+            exception.setTopLevelKeyword(topLevelKeyword);
+
+            throw exception;
+        }
+    }
+
+
+    public List<String> getSectionList(String topLevelKeyword) {
+        return new QueryStringUtil(this.query.getQueryString().getQuery())
+                .getTopLevelSectionsByKeyword(topLevelKeyword);
     }
 
 }
