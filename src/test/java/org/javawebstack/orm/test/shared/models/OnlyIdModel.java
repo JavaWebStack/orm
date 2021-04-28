a770745c8465177d0d4383eaf4e892ace60930a3
TimothyGillespie <Timothy.Gillespie@outlook.de>
2021-04-28 23:46:52 +0200

    Add Getter to OnlyIdModel

--- src/test/java/org/javawebstack/orm/test/shared/models/OnlyIdModel.java
+++ src/test/java/org/javawebstack/orm/test/shared/models/OnlyIdModel.java
@@ -1,8 +1,10 @@
 package org.javawebstack.orm.test.shared.models;
 
+import lombok.Getter;
 import org.javawebstack.orm.Model;
 import org.javawebstack.orm.annotation.Column;
 
+@Getter
 public class OnlyIdModel extends Model {
 
     @Column
