0f6e7d130225bcc4632711b39b6f46f84b383e0d
JanHolger <jan@bebendorf.eu>
2021-09-01 09:24:42 +0200

    Improved the default filter

--- src/main/java/org/javawebstack/orm/filter/DefaultQueryFilter.java
+++ src/main/java/org/javawebstack/orm/filter/DefaultQueryFilter.java
@@ -1,8 +1,11 @@
 package org.javawebstack.orm.filter;
 
 import org.javawebstack.orm.Model;
+import org.javawebstack.orm.TableInfo;
 import org.javawebstack.orm.query.Query;
+import org.javawebstack.orm.util.Helper;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +20,36 @@ public class DefaultQueryFilter implements QueryFilter {
     }
 
     public void filter(Query<? extends Model> query, Map<String, String> filter) {
+        TableInfo info = query.getRepo().getInfo();
         query.and(q -> {
-            filter.keySet().stream().filter(filterable.keySet()::contains).forEach(key -> q.where(filterable.get(key), filter.get(key)));
+            filter.keySet().stream().filter(filterable.keySet()::contains).forEach(key -> {
+                boolean not = false;
+                if(key.endsWith("!")) {
+                    key = key.substring(0, key.length()-1);
+                    not = true;
+                }
+                key = filterable.get(key);
+                String v = filter.get(key);
+                Field field = info.getField(key);
+                if(field == null) {
+                    key = Helper.toCamelCase(key);
+                    field = info.getField(key);
+                }
+                if(field == null)
+                    return;
+                if(v.equals("null")) {
+                    if(not)
+                        q.whereNull(key);
+                    else
+                        q.whereNotNull(key);
+                    return;
+                }
+                if(field.getType().equals(Boolean.class) || field.getType().equals(boolean.class)) {
+                    q.where(key, not ? "!=" : "=", v.equals("1") || v.equals("true"));
+                    return;
+                }
+                q.where(key, not ? "!=" : "=", v);
+            });
             return q;
         });
     }
--- src/main/java/org/javawebstack/orm/util/Helper.java
+++ src/main/java/org/javawebstack/orm/util/Helper.java
@@ -21,4 +21,23 @@ public class Helper {
         return Character.toLowerCase(source.charAt(0)) + source.substring(1);
     }
 
+    public static String toCamelCase(String source) {
+        if(source == null || source.length() == 0)
+            return source;
+        StringBuilder sb = new StringBuilder()
+                .append(Character.toLowerCase(source.charAt(0)));
+        boolean wordFlag = false;
+        for(int i=1; i<source.length(); i++) {
+            char c = source.charAt(i);
+            if(c == '-' || c == '_') {
+                wordFlag = true;
+                continue;
+            }
+            if(Character.isUpperCase(c) && Character.isLowerCase(source.charAt(i-1)))
+                wordFlag = true;
+            sb.append(wordFlag ? Character.toUpperCase(c) : Character.toUpperCase(c));
+        }
+        return sb.toString();
+    }
+
 }
