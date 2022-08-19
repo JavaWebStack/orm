309e612c732ceee640ea90d3edb33c8998736ccd
j.bebendorf <j.bebendorf@lumaserv.com>
2022-08-19 12:14:46 +0200

    Fixed data type for abstract data json mapper

--- src/main/java/org/javawebstack/orm/mapper/AbstractDataTypeMapper.java
+++ src/main/java/org/javawebstack/orm/mapper/AbstractDataTypeMapper.java
@@ -4,6 +4,15 @@ import org.javawebstack.abstractdata.AbstractElement;
 import org.javawebstack.orm.SQLType;
 
 public class AbstractDataTypeMapper implements TypeMapper {
+
+    private static final int BYTES_OVERHEAD_TEXT = 2;
+    private static final int BYTES_OVERHEAD_MEDIUMTEXT = 3;
+    private static final int BYTES_OVERHEAD_LONGTEXT = 4;
+
+    private static final long MAX_SIZE_TEXT = (long) Math.floor((65535 - BYTES_OVERHEAD_TEXT) / 4);
+    private static final long MAX_SIZE_MEDIUMTEXT = (long) Math.floor((16777215 - BYTES_OVERHEAD_MEDIUMTEXT) / 4);
+    private static final long MAX_SIZE_LONGTEXT = (long) Math.floor((4294967295L - BYTES_OVERHEAD_LONGTEXT) / 4);
+
     public Object mapToSQL(Object source, Class<?> type) {
         if (source == null)
             return null;
@@ -21,8 +30,13 @@ public class AbstractDataTypeMapper implements TypeMapper {
     }
 
     public SQLType getType(Class<?> type, int size) {
-        if (AbstractElement.class.isAssignableFrom(type))
+        if (AbstractElement.class.isAssignableFrom(type)) {
+            if (size > MAX_SIZE_MEDIUMTEXT)
+                return SQLType.LONGTEXT;
+            if (size > MAX_SIZE_TEXT)
+                return SQLType.MEDIUMTEXT;
             return SQLType.TEXT;
+        }
         return null;
     }
 
