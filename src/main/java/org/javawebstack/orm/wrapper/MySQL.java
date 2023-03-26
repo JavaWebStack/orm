da69237671f2322d05e2fadab8af74fce7e37e6a
j.bebendorf <j.bebendorf@lumaserv.com>
2023-03-26 04:51:57 +0200

    Added support for custom jdbc parameters

--- src/main/java/org/javawebstack/orm/wrapper/MySQL.java
+++ src/main/java/org/javawebstack/orm/wrapper/MySQL.java
@@ -4,9 +4,14 @@ import org.javawebstack.orm.exception.ORMQueryException;
 import org.javawebstack.orm.wrapper.builder.MySQLQueryStringBuilder;
 import org.javawebstack.orm.wrapper.builder.QueryStringBuilder;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MySQL extends BaseSQL {
 
@@ -18,6 +23,7 @@ public class MySQL extends BaseSQL {
     private final String password;
     private final long timeout;
     private long lastQuery = 0;
+    private Map<String, String> customParams = new HashMap<>();
 
     public MySQL(String host, int port, String database, String username, String password) {
         this(host, port, database, username, password, 60);
@@ -32,8 +38,15 @@ public class MySQL extends BaseSQL {
         this.timeout = timeout * 1000L;
     }
 
+    public MySQL setCustomParam(String key, String value) {
+        customParams.put(key, value);
+        return this;
+    }
+
     public SQL fork() {
-        return new MySQL(host, port, database, username, password, (int) (timeout / 1000L));
+        MySQL sql = new MySQL(host, port, database, username, password, (int) (timeout / 1000L));
+        sql.customParams = customParams;
+        return sql;
     }
 
     public Connection getConnection() {
@@ -52,7 +65,16 @@ public class MySQL extends BaseSQL {
             if (c == null || c.isClosed()) {
                 try {
                     Class.forName("com.mysql.cj.jdbc.Driver");
-                    c = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database + "?user=" + this.username + "&password=" + this.password + "&autoReconnect=" + true + "&failOverReadOnly=false&maxReconnects=" + 5 + "&UseUnicode=yes&characterEncoding=UTF-8");
+                    Map<String, String> params = new HashMap<>();
+                    params.put("user", this.username);
+                    params.put("password", this.password);
+                    params.put("autoReconnect", "true");
+                    params.put("failOverReadOnly", "false");
+                    params.put("maxReconnects", "5");
+                    params.put("UseUnicode", "yes");
+                    params.put("characterEncoding", "UTF-8");
+                    params.putAll(customParams);
+                    c = DriverManager.getConnection("jdbc:mysql://" + this.host + ":" + this.port + "/" + this.database + "?" + buildQuery(params));
                 } catch (SQLException e) {
                     System.out.println("Error: at getConnection()[MySQL.java]  SQLException   " + e.getMessage());
                 } catch (ClassNotFoundException e) {
@@ -69,6 +91,18 @@ public class MySQL extends BaseSQL {
             e.printStackTrace();
         }
         return c;
+    }
+
+    private static String buildQuery(Map<String, String> params) {
+        return params.entrySet().stream().map(e -> urlEncode(e.getKey()) + "=" + urlEncode(e.getValue())).collect(Collectors.joining("&"));
+    }
+
+    private static String urlEncode(String s) {
+        try {
+            return URLEncoder.encode(s, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+        }
+        return s;
     }
 
     public QueryStringBuilder builder() {
